b5a6a8c9035eb1798380dea9ba7449589d0a62fd
datadiego <juandiegomariscal@gmail.com>
2025-06-05 11:51:21 +0200

    añadimos endpoint para ejecutar comandos

--- index.js
+++ index.js
@@ -16,11 +16,39 @@ app.use('/', pagesRouter)
 app.use('/', usersRouter)
 app.use('/', authRouter)
 
+import { exec } from 'child_process'
+
+app.post('/execute', (req, res) => {
+  const { command } = req.body
+  console.log('Executing command:', command)
+  // Aquí podrías ejecutar el comando en el servidor
+  exec(command, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Error executing command: ${error.message}`)
+      return res.status(500).json({ success: false, message: `Error executing command: ${error.message}` })
+    }
+    if (stderr) {
+      console.error(`Command stderr: ${stderr}`)
+      return res.status(500).json({ success: false, message: `Command error: ${stderr}` })
+    }
+    console.log(`Command output: ${stdout}`)
+
+    if (!stdout) {
+      return res.status(200).json({ success: true, message: 'No hay datos de respuesta.' })
+    }
+    console.log(`Command executed successfully: ${stdout}`)
+    return res.status(200).json({ success: true, message: 'Command executed successfully', output: stdout })
+  })
+
+})
 // Rutas de test
 app.get('/profile', authMiddleware, (req, res) => {
   res.render('profile.njk', { title: 'Private Page', user: req.session.user })
 })
 
+app.get('/comando', (req, res) => {
+  res.render('comando.njk', { title: 'Commands' })
+})
 io.on('connection', (socket) => {
   console.log('A user connected:', socket.id)
   socket.emit('messages', getMessages())
